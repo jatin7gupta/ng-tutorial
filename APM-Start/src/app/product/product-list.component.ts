5699d0ff66e8a785931e3d5978e5d0de2110a7ea
jatin7gupta <jatin13csu055@ncuindia.edu>
2017-09-08 12:27:41 +0530

    Add: show image

--- APM-Start/src/app/product/product-list.component.ts
+++ APM-Start/src/app/product/product-list.component.ts
@@ -31,4 +31,8 @@ export class ProductListComponent {
   ];
   imageWidth: number = 50;
   imageMargin: number = 2;
+  showImage: boolean = false;
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
